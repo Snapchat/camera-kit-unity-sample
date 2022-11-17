df66f1080609fc37953f011b9ff6c4840c0fee84
Edgar Neto <eneto@snapchat.com>
2022-11-17 20:28:01 +0000

    Added Unity ios build settings

---
+++ unity-ios/unity-ios/unity_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  unity_iosApp.swift
+//  unity-ios
+//
+//  Created by Edgar Neto on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct unity_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ unity-ios/unity-iosUITests/unity_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  unity_iosUITestsLaunchTests.swift
+//  unity-iosUITests
+//
+//  Created by Edgar Neto on 17/11/2022.
+//
+
+import XCTest
+
+final class unity_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
