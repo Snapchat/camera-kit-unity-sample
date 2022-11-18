7a19252526e3e32a84cf6a03bd1965c29097d196
Edgar Neto <eneto@snapchat.com>
2022-11-18 11:38:45 +0000

    Added new iOS native project with storyboard

---
+++ unity-ios/unity-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unity-ios
+//
+//  Created by Edgar Neto on 18/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
